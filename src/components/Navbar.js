27bec95c2e46edcec748b5eaf2b3a3ffcd9ca31e
saralynleonard <saralynl@bu.edu>
2025-06-09 18:19:05 -0400

    changed BrowserRouter to HashRouter

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import { Link, NavLink } from 'react-router-dom';
 function Navbar() {
     return ( 
         <nav className="navbar">
-            <NavLink to="/react-personal-site" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>About Me</NavLink>
-            <NavLink to="/react-personal-site/projects" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>Projects</NavLink>
-            <NavLink to="/react-personal-site/experience" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>Experience</NavLink>
-            <NavLink to="/react-personal-site/education" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>Education</NavLink>
+            <NavLink to="/" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>About Me</NavLink>
+            <NavLink to="/projects" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>Projects</NavLink>
+            <NavLink to="/experience" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>Experience</NavLink>
+            <NavLink to="/education" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>Education</NavLink>
         </nav>
     )
 }
